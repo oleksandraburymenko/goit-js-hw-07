637e034b93134559a7d1508cad831cc73ab23891
oleksandraburymenko <115561772+oleksandraburymenko@users.noreply.github.com>
2023-03-03 22:25:04 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+gallery.addEventListener('click', onImgClick);
+
+function onImgClick (e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return;
+    }
+    const modal = basicLightbox.create(
+    `<div class="modal">
+    </div>`);
+
+modal.show()
+}
+
+const createGalleryGrid = ({ preview, original, description }) => 
+`<div class="gallery__item">
+<a class="gallery__link" href="large-image.jpg">
+  <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+  />
+</a>
+</div>`;
+
+const markupGallery = galleryItems.map(e => createGalleryGrid(e)).join('');
+gallery.insertAdjacentHTML('afterbegin', markupGallery);
